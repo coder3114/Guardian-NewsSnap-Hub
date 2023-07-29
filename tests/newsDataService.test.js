06e42a1fa50bdcced311c2a399e362acf0cf2f4d
coderella <qiangie.yang@gmail.com>
2023-07-29 20:49:50 +0100

    Update API link using the key

--- tests/newsDataService.test.js
+++ tests/newsDataService.test.js
@@ -5,14 +5,17 @@ import testData from "./testData.json";
 // eslint-disable-next-line no-undef
 vi.mock(`axios`);
 
-describe(`getNews tests - GEt request tests to /response`, () => {
+describe(`getNews tests - GET request tests to /response`, () => {
   it("1 - should should actually make the external data call", async () => {
     // Arrange
     axios.get.mockResolvedValueOnce({ data: testData });
     // Act
     await getNews();
     // Assert
-    expect(axios.get).toHaveBeenCalledWith(`http://localhost:3000/response`);
+    // expect(axios.get).toHaveBeenCalledWith(`http://localhost:3000/response`);
+    expect(axios.get).toHaveBeenCalledWith(
+      `https://content.guardianapis.com/search?order-by=newest&show-fields=byline%2Cthumbnail%2Cheadline%2CbodyText&api-key=cfbd00ae-c12a-49b7-b4c8-be4f1e29b55d`
+    );
   });
 
   it("2 - should have successful request returning the right data", async () => {
@@ -21,7 +24,7 @@ describe(`getNews tests - GEt request tests to /response`, () => {
     // Act
     const result = await getNews();
     // Assert
-    expect(result).toEqual(testData.results);
+    expect(result).toEqual(testData.response.results);
   });
 
   it("3 - should have unsuccessful request returning the error object", async () => {
